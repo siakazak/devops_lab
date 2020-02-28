8b916f76e310b5cbd102feba57a210ec6610f69f
Siarhei Kazak <siarhei_kazak@epam.com>
2020-02-28 15:34:05 +0300

    cleanup

--- python_create_env.sh
+++ python_create_env.sh
@@ -2,10 +2,14 @@
 
 #!/bin/bash
 
+# Logfile can be specified here
+
+LOGFILE="/dev/null"
+
 VERSIONS="$@"
 
 function usage() {
-	echo -e "\nUsage: \n$0 <VERSION> <VERSION>"
+	echo -e "\nUsage: \n$0 [<VERSION_1> <VERSION_2> .. <VERSION_N>]"
 	echo -e "\nDefault values: 2.7.17 3.8.1"
 	echo "Script will proceed in 5 seconds..."
 	echo -e "<CTRL-C> to cancel\n"
@@ -14,9 +18,9 @@ function usage() {
 # Check arguments
 
 if [[ "$#" == 0  ]]; then
-	echo "*********************************************************"
+	echo "****************************************************"
 	echo -e "*WARN* - Script takes valid python versions as args"
-	echo "*********************************************************"
+	echo "****************************************************"
 	usage
 	sleep 6s
 	VERSIONS=("2.7.17" "3.8.1")
@@ -24,15 +28,22 @@ fi
 
 NAME_PREFIX="testenv"
 
+touch $LOGFILE
+
 # Install versions and create virtualenvs
 
 for V in ${VERSIONS[@]}; do
 	NAME="$NAME_PREFIX-$V"
-	pyenv install -s $V >/dev/null
-	pyenv virtualenv $V $NAME
+	pyenv install -s $V >> $LOGFILE && echo -e "ok\n" || echo -e "\nERROR - not created"
+	echo -e "Creating virtualenv for Python-$V...\n"
+	pyenv virtualenv $V $NAME >> $LOGFILE && echo -e "ok\n" || echo -e "\nERROR - not created"
 	pyenv local $NAME
+	echo "*****************************************"
 	echo -e "\nvirtualenv $(pyenv local) is using:"
  	echo "$(python -V)"
+	echo "*****************************************"
 done
+
+echo "Done"
 
 exit 0
