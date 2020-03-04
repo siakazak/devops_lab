4b15fc4b4327b3fd083db4a58d73f083972eae34
flyboy14 <twiceaday28@gmail.com>
2020-03-05 02:19:57 +0300

    sugar

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="pr-stats",
     packages=find_packages(),
-    scripts=["pr-stats"],
-    version="0.2",
+    scripts=["pr-stats", "githubuser.py"],
+    version="0.2.8",
     author="Siarhei Kazak",
     author_email="siarhei_kazak@epam.com",
     description="Demo GitHub REST API script",
